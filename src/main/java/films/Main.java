18da1119c8ab6f204235eb87b7f743c0fc4dc96f
Gluglug7 <abod278@aucklanduni.ac.nz>
2023-12-29 12:58:17 +1300

    Adding interactions into the CommandLineInterface class
    Adding methods to print the details required for the turn and to read and check the user's guess for that turn. Currently only works for a single turn, needs to be reworked so that each turn more information is given and after each turn the program stops to read the input rather than thinking there is none (excluding the first turn)

--- src/main/java/films/Main.java
+++ src/main/java/films/Main.java
@@ -1,8 +1,6 @@
 package films;
 
-import films.objects.json.FilmInfo;
 import films.services.GetFilmDetailsService;
-import films.ui.UserInterface;
 import films.ui.cli.CommandLineInterface;
 import films.util.Config;
 
@@ -10,24 +8,25 @@ public class Main {
   public static void main(String[] args) {
     Config.read();
 
-    UserInterface ui = new CommandLineInterface();
-    ui.start();
-
     // The Shawshank Redemption
     String json = new GetFilmDetailsService("tt0111161").send();
-    FilmInfo film = new FilmInfo(json);
-    System.out.println(
-        "\n"
-            + film.getTitle()
-            + " ("
-            + film.getYear()
-            + ")"
-            + ", directed by "
-            + film.getDirectors()
-            + ", written by "
-            + film.getWriters()
-            + ", starring "
-            + film.getCast()
-            + ".\n");
+
+    CommandLineInterface ui = new CommandLineInterface(json);
+    ui.start();
+
+    // FilmInfo film = ui.film;
+    // System.out.println(
+    //     "\n"
+    //         + film.getTitle()
+    //         + " ("
+    //         + film.getYear()
+    //         + ")"
+    //         + ", directed by "
+    //         + film.getDirectors()
+    //         + ", written by "
+    //         + film.getWriters()
+    //         + ", starring "
+    //         + film.getCast()
+    //         + ".\n");
   }
 }
--- src/main/java/films/ui/cli/CommandLineInterface.java
+++ src/main/java/films/ui/cli/CommandLineInterface.java
@@ -1,11 +1,76 @@
 package films.ui.cli;
 
+import films.objects.json.FilmInfo;
 import films.ui.UserInterface;
+import java.util.Scanner;
 
 public class CommandLineInterface implements UserInterface {
+  private static final int MAX_TURNS = 8;
 
+  public FilmInfo film;
+  public int turn;
+
+  /**
+   * Creates a CommandLineInterface object from a JSON string. Creates a FilmInfo object from the
+   * JSON string. Sets the turn to 1 as the game has just started.
+   *
+   * @param json The JSON string to extract the data from.
+   */
+  public CommandLineInterface(String json) {
+    this.film = new FilmInfo(json);
+    this.turn = 1;
+  }
+
+  // TODO: Make this (and readGuess() method) work repeatedly until the user gets it right or they
+  // run out of turns
   @Override
   public void start() {
-    System.out.println("CLI started!");
+    printDetails();
+    if (readGuess(film.getTitle())) {
+      System.out.println("Correct!");
+    } else {
+      System.out.println("Incorrect!");
+    }
+
+    // while (turn <= MAX_TURNS) {
+    //   printDetails();
+    //   if (readGuess(film.getTitle())) {
+    //     System.out.println("Correct!");
+    //     return;
+    //   } else {
+    //     System.out.println("Incorrect!");
+    //     turn++;
+    //   }
+    // }
+  }
+
+  /**
+   * Reads the user's guess from the command line and compares it to the movie's title.
+   *
+   * @return true if the user's guess is correct, false otherwise
+   */
+  public boolean readGuess(String title) {
+    Scanner scanner = new Scanner(System.in);
+    String guess = scanner.nextLine().trim().replaceAll(" ", "").replaceAll("-", "");
+    title = title.toLowerCase().replaceAll(" ", "").replaceAll("-", "");
+    scanner.close();
+    return guess.toLowerCase().equals(title);
+  }
+
+  // TODO: Print details based on the turn only
+  /**
+   * Prints the information the player needs to know for the turn. Asks for the movie's name after
+   * printing details.
+   */
+  public void printDetails() {
+    System.out.println("\nTurn " + turn + ":");
+    System.out.println("Release year: " + film.getYear());
+    System.out.println("Rating: " + film.getRated());
+    System.out.println("Runtime: " + film.getRuntime());
+    System.out.println("Genre(s): " + film.getGenre());
+    System.out.println("Director(s): " + film.getDirectors());
+    System.out.println("Writer(s): " + film.getWriters());
+    System.out.println("Cast: " + film.getCast());
+    System.out.println("\nName the movie: ");
   }
 }
