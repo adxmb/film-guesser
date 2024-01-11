04bae211ec1707c14e4a0df0d78307e4fa61b92e
Oculux <nate.williamson.nz@gmail.com>
2024-01-11 14:29:59 +1300

    :art: fill out UserInterface interface

--- src/main/java/films/ui/UserInterface.java
+++ src/main/java/films/ui/UserInterface.java
@@ -1,6 +1,45 @@
 package films.ui;
 
+import films.State;
+import films.objects.json.FilmInfo;
+
 public interface UserInterface {
-  // Add all UI methods here to allow easy switching between UIs
+  /** Place for miscellaneous setup tasks. Should be called at the start of the game. */
   void start();
+
+  /**
+   * Asks the user to choose a difficulty level.
+   *
+   * @return The difficulty level chosen by the user.
+   */
+  State.Difficulty askDifficulty();
+
+  /** Displays the introduction to the game. */
+  void showIntroduction();
+
+  /**
+   * Displays the current turn and the corresponding information about the movie available to the
+   * user.
+   *
+   * @param film The movie whose details are to be displayed.
+   * @param turn The current turn number.
+   */
+  void showFilmDetails(FilmInfo film, int turn);
+
+  /**
+   * Asks the user to guess the movie.
+   *
+   * @return The user's guess.
+   */
+  String askGuess();
+
+  /** Displays the win message. */
+  void showWin();
+
+  /**
+   * Displays the lose message, including the correct title of the movie.
+   *
+   * @param film The movie whose title is to be displayed.
+   */
+  void showLose(FilmInfo film);
 }
--- src/main/java/films/ui/cli/CommandLineInterface.java
+++ src/main/java/films/ui/cli/CommandLineInterface.java
@@ -1,5 +1,7 @@
 package films.ui.cli;
 
+import films.State.Difficulty;
+import films.objects.json.FilmInfo;
 import films.ui.UserInterface;
 
 public class CommandLineInterface implements UserInterface {
@@ -7,5 +9,41 @@ public class CommandLineInterface implements UserInterface {
   public void start() {
     new GameSelect().askDifficulty();
     System.out.println("Welcome to the Films game!");
+  }
+
+  @Override
+  public Difficulty askDifficulty() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'askDifficulty'");
+  }
+
+  @Override
+  public void showIntroduction() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'showIntroduction'");
+  }
+
+  @Override
+  public void showFilmDetails(FilmInfo film, int turn) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'showFilmDetails'");
+  }
+
+  @Override
+  public String askGuess() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'askGuess'");
+  }
+
+  @Override
+  public void showWin() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'showWin'");
+  }
+
+  @Override
+  public void showLose(FilmInfo film) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'showLose'");
   }
 }
