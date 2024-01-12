f25c0287c034360d5430a0b93e45b2c8b5e67ddf
Oculux <nate.williamson.nz@gmail.com>
2024-01-12 17:33:18 +1300

    :sparkles: implement automatic game restart on gameover

--- src/main/java/films/Main.java
+++ src/main/java/films/Main.java
@@ -4,9 +4,15 @@ import films.ui.cli.CommandLineInterface;
 import films.util.Config;
 
 public class Main {
+
   public static void main(String[] args) {
     Config.read();
-    Game game = new Game(new CommandLineInterface());
-    game.start();
+    while (true) {
+      restartGame();
+    }
+  }
+
+  public static void restartGame() {
+    new Game(new CommandLineInterface()).start();
   }
 }
